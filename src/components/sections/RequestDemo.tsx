e31dc32d7efcb2eb54a3642a67eca4ebe6780d48
carnage999-max <jamesezekiel039@gmail.com>
2026-01-28 05:57:49 +0100

    chore: updated usage of recaptcha keys in forms

--- src/components/sections/RequestDemo.tsx
+++ src/components/sections/RequestDemo.tsx
@@ -180,7 +180,7 @@ export function RequestDemo() {
                         <div className="mb-8 flex justify-center">
                             <ReCAPTCHA
                                 ref={captchaRef}
-                                sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI" // Test key
+                                sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"} // Use env var or test key
                                 theme="dark"
                                 onChange={(val) => setValue("captcha", val || "")}
                             />
